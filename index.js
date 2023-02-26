40cd7bbba3517919177ee89bd83a20c6937b0c58
EugeneSemivolos <semivolos.evgen1@gmail.com>
2023-02-26 23:07:35 +0200

    Revert "inplemented interactive mode"
    This reverts commit f26977e0ffd4be9c3e14c64999b0f9e35115ce1f.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import startInteractiveMode from "./interactiveMode.js";
-
 console.log('Which mode do you want to turn on?(interactive or file)');
 process.stdin.on('data', data => {
   const workingMode = data.toString().trim();
   
   if (workingMode === 'interactive') {
-    startInteractiveMode();
+    console.log('Interactive mode is activated!');
   } else if (workingMode === 'file') {
     console.log('File mode is activated!');
   } else {
